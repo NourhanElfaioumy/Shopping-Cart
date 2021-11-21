001b48f6594683410df65a96e9f452ee7395881c
NourhanElfaioumy <60580404+NourhanElfaioumy@users.noreply.github.com>
2021-11-22 00:18:09 +0200

    Cart list Edit

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,100 +1,25 @@
-let productsInCart = JSON.parse(localStorage.getItem("shoppingCart"));
-if (!productsInCart) {
-  productsInCart = [];
-}
-const parentElement = document.querySelector("#buyItems");
-const cartSumPrice = document.querySelector(".list-cart");
-const products = document.querySelectorAll(".product-cards");
-
-const countTheSumPrice = function () {
-  let sum = 0;
-  productsInCart.forEach((item) => {
-    sum += item.price;
+let carts = document.querySelectorAll(".card-btn");
+for (let i = 0; i < carts.length; i++) {
+  carts[i].addEventListener("click", () => {
+    cartNumber();
   });
-  return sum;
-};
+}
+function onLoadCartNumber() {
+  let productNumber = localStorage.getItem("cartNumber");
+  if (productNumber) {
+    document.querySelector(".cart-span").textContent = productNumber;
+  }
+}
 
-const updateShoppingCartHTML = function () {
-  localStorage.setItem("shoppingCart", JSON.stringify(productsInCart));
-  if (productsInCart.length > 0) {
-    let result = productsInCart.map((product) => {
-      return `
-				<li class="buyItem">
-					<img src="${product.image}">
-					<div>
-						<h5>${product.name}</h5>
-						<h6>$${product.price}</h6>
-						<div>
-							<button class="button-minus" data-id=${product.id}>-</button>
-							<span class="countOfProduct">${product.count}</span>
-							<button class="button-plus" data-id=${product.id}>+</button>
-						</div>
-					</div>
-				</li>`;
-    });
-    parentElement.innerHTML = result.join("");
-    document.querySelector(".checkout").classList.remove("hidden");
-    cartSumPrice.innerHTML = "$" + countTheSumPrice();
+function cartNumber() {
+  let productNumber = localStorage.getItem("cartNumber");
+  productNumber = parseInt(productNumber);
+  if (productNumber) {
+    localStorage.setItem("cartNumber", productNumber + 1);
+    document.querySelector(".cart-span").textContent = productNumber + 1;
   } else {
-    document.querySelector(".checkout").classList.add("hidden");
-    parentElement.innerHTML =
-      '<h4 class="empty">Your shopping cart is empty</h4>';
-    cartSumPrice.innerHTML = "";
+    localStorage.setItem("cartNumber", 1);
+    document.querySelector(".cart-span").textContent = 1;
   }
-};
-
-function updateProductsInCart(product) {
-  for (let i = 0; i < productsInCart.length; i++) {
-    if (productsInCart[i].id == product.id) {
-      productsInCart[i].count += 1;
-      productsInCart[i].price =
-        productsInCart[i].basePrice * productsInCart[i].count;
-      return;
-    }
-  }
-  productsInCart.push(product);
 }
-
-products.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    if (e.target.classList.contains("addToCart")) {
-      const productID = e.target.dataset.productId;
-      const productName = item.querySelector(".product-info").innerHTML;
-      const productPrice = item.querySelector(".price").innerHTML;
-      const productImage = item.querySelector("img").src;
-      let product = {
-        name: productName,
-        image: productImage,
-        id: productID,
-        count: 1,
-        price: +productPrice,
-        basePrice: +productPrice,
-      };
-      updateProductsInCart(product);
-      updateShoppingCartHTML();
-    }
-  });
-});
-parentElement.addEventListener("click", (e) => {
-  const isPlusButton = e.target.classList.contains("button-plus");
-  const isMinusButton = e.target.classList.contains("button-minus");
-  if (isPlusButton || isMinusButton) {
-    for (let i = 0; i < productsInCart.length; i++) {
-      if (productsInCart[i].id == e.target.dataset.id) {
-        if (isPlusButton) {
-          productsInCart[i].count += 1;
-        } else if (isMinusButton) {
-          productsInCart[i].count -= 1;
-        }
-        productsInCart[i].price =
-          productsInCart[i].basePrice * productsInCart[i].count;
-      }
-      if (productsInCart[i].count <= 0) {
-        productsInCart.splice(i, 1);
-      }
-    }
-    updateShoppingCartHTML();
-  }
-});
-
-updateShoppingCartHTML();
+onLoadCartNumber();
