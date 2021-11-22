b29255855132f45b895e5d98dea583a326184eb6
NourhanElfaioumy <60580404+NourhanElfaioumy@users.noreply.github.com>
2021-11-22 02:51:04 +0200

    shopping cart 2

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,3 +1,52 @@
+// let products = [
+//   {
+//     id: 1,
+//     title: "Brand",
+//     image: "img/card2.png",
+//     description: "T-shirt and Sneaker",
+//     price: "120$",
+//   },
+//   {
+//     id: 2,
+
+//     title: "Brand",
+//     image: "img/product-8.jpg",
+//     description: "Smart Watch",
+//     price: "220$",
+//   },
+//   {
+//     id: 3,
+//     title: "Brand",
+//     image: "img/card5.png",
+//     description: "T-shirt",
+//     price: "$53$",
+//   },
+//   {
+//     id: 4,
+//     title: "Brand",
+//     image: "img/card9.png",
+//     description: "Black & White Sneaker",
+//     price: "225$",
+//   },
+//   {
+//     id: 5,
+//     title: "Brand",
+//     image: "img/product-9.jpg",
+//     description: "Smart Watch",
+//     price: "120$",
+//   },
+//   {
+//     id: 6,
+//     title: "Brand",
+//     image: "img/card11.png",
+//     description: "Grey & Orange Sneaker",
+//     price: "200$",
+//   },
+// ];
+
+let productDom = document.querySelector(".product-cards");
+let cartProduct = document.querySelector(".carts-product");
+
 let carts = document.querySelectorAll(".card-btn");
 for (let i = 0; i < carts.length; i++) {
   carts[i].addEventListener("click", () => {
@@ -22,4 +71,31 @@ function cartNumber() {
     document.querySelector(".cart-span").textContent = 1;
   }
 }
-onLoadCartNumber();
+// onLoadCartNumber();
+
+// function productUI() {
+//   let productUII = products.map((item) => {
+//     return `
+//         <div  class="product-cards">
+//         <div class="product-img">
+//             <img src='${item.image}' class="image-product" >
+//         <button class="card-btn" onclick='addToCart(${item.id})'>Add to cart</button>
+//         </div>
+//         <div class="product-info">
+//             <h2 class="product-brand">'${item.title}'</h2>
+//             <p class="product-description">'${item.description}'</p>
+//         <span class="price">'${item.price}'</span>
+//         <span class="price-span"></span>
+
+//     </div>
+//     <div>
+//         `;
+//   });
+//   productDom.innerHTML = productUII;
+// }
+// productUI();
+
+// function addToCart(id) {
+//   let chooseItem = products.find((item) => item.id === id);
+//   cartProduct.innerHTML += `<p>${chooseItem.description}</p>`;
+// }
